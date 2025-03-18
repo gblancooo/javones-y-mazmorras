bf71eefb407a46302ca574a06efca1107ad9921f
gblancooo <gerardblanco2006@gmail.com>
2025-03-18 19:18:59 +0100

    Objeto equipamiento

--- dragones_y_mazmorras/src/dragones_y_mazmorras/Equipamiento.java
+++ dragones_y_mazmorras/src/dragones_y_mazmorras/Equipamiento.java
@@ -1,5 +1,29 @@
 package dragones_y_mazmorras;
 
-public class Equipamiento {
+public abstract class Equipamiento {
 
+	private String nombre;
+	private String tipo;
+	
+	public Equipamiento(String nombre, String tipo){
+		this.nombre = nombre;
+		this.tipo = tipo;
+	}
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+	
 }
