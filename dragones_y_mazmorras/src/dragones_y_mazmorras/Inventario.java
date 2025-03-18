dc54d30e6efc4f3c6c8f27e65b5db9cd9c052964
gblancooo <gerardblanco2006@gmail.com>
2025-03-18 19:43:07 +0100

    Equipamiento + Inventario

--- dragones_y_mazmorras/src/dragones_y_mazmorras/Inventario.java
+++ dragones_y_mazmorras/src/dragones_y_mazmorras/Inventario.java
@@ -1,5 +1,36 @@
 package dragones_y_mazmorras;
 
-public class Inventario {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class Inventario {
+	
+	private ArrayList<Equipamiento> equipamientos;
+	
+	public Inventario(String nombre, String tipo) {
+		
+		this.equipamientos = new ArrayList<>();
+	}
+
+
+	
+	
+	public void agregarEquipamiento(Equipamiento equipamiento) {
+		
+		this.equipamientos.add(equipamiento);
+		
+	}
+	
+	public void mostrarInventario(Equipamiento equipamiento) {
+		
+		for(Equipamiento i: equipamientos) {
+			System.out.println("Objeto 1: " + i.getNombre() + " Tipo: " + i.getTipo());
+			
+		}
+		
+		
+		
+	}
+	
+	
 }
