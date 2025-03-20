ab1b9c8de64832c746fd51d1d30712d47f9b3917
Aarongp13 <aarongarcia132005@gmail.com>
2025-03-20 16:38:09 +0100

    introducir getter y setters + atributos

--- dragones_y_mazmorras/src/dragones_y_mazmorras/Guerrero.java
+++ dragones_y_mazmorras/src/dragones_y_mazmorras/Guerrero.java
@@ -1,5 +1,52 @@
 package dragones_y_mazmorras;
+	
+	
+	public class Guerrero extends Personaje {
+		
+		private boolean protegiendo;
+		private boolean compañeroProtegido;
+		
+	    public Guerrero(String nombre, int nivel, int puntosVida, boolean protegiendo, boolean compañeroProtegido) {
+			super(nombre, nivel, puntosVida);
+			this.setProtegiendo(protegiendo);
+			this.setCompañeroProtegido(compañeroProtegido);
+			
+		}
 
-public class Guerrero {
+		public boolean isProtegiendo() {
+			return protegiendo;
+		}
 
-} 
+		public void setProtegiendo(boolean protegiendo) {
+			this.protegiendo = protegiendo;
+		}
+
+		public boolean isCompañeroProtegido() {
+			return compañeroProtegido;
+		}
+
+		public void setCompañeroProtegido(boolean compañeroProtegido) {
+			this.compañeroProtegido = compañeroProtegido;
+		}
+
+		public void proteger(Personaje compañero) {
+		}
+		
+		
+		public void dejarDeProteger() {
+		}
+		
+		
+		public void bajarVida(int vida) {
+		}
+		
+		
+		
+		public void estaProtegiendo() {
+		}
+	
+		
+	
+		}
+		
+	
